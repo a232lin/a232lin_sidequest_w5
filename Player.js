964e9249245cdf2fdef5e633f724f14d5007d0de
Angela Lin <a232lin@uwaterloo.ca>
2026-02-15 01:08:12 -0500

    added faces

--- Player.js
+++ Player.js
@@ -6,15 +6,36 @@ class Player {
   }
 
   updateInput() {
-    if (keyIsDown(LEFT_ARROW)) this.x -= this.speed;
-    if (keyIsDown(RIGHT_ARROW)) this.x += this.speed;
-    if (keyIsDown(UP_ARROW)) this.y -= this.speed;
-    if (keyIsDown(DOWN_ARROW)) this.y += this.speed;
+    if (keyIsDown(LEFT_ARROW) || keyIsDown(65)) this.x -= this.speed; // A
+    if (keyIsDown(RIGHT_ARROW) || keyIsDown(68)) this.x += this.speed; // D
+    if (keyIsDown(UP_ARROW) || keyIsDown(87)) this.y -= this.speed; // W
+    if (keyIsDown(DOWN_ARROW) || keyIsDown(83)) this.y += this.speed; // S
   }
 
   draw() {
+    push();
+    translate(this.x, this.y);
+
+    // Body
     noStroke();
-    fill(255, 200, 100);
-    ellipse(this.x, this.y, 20);
+    fill(255, 165, 0); // orange
+    ellipse(0, 0, 30);
+
+    // Eyes (oval)
+    fill(255);
+    ellipse(-6, -5, 6, 10); // left
+    ellipse(6, -5, 6, 10); // right
+
+    fill(0);
+    ellipse(-6, -5, 3, 6); // left pupil
+    ellipse(6, -5, 3, 6); // right pupil
+
+    // Smiling mouth
+    noFill();
+    stroke(0);
+    strokeWeight(2);
+    arc(0, 3, 15, 10, 0, PI); // smile
+
+    pop();
   }
 }
--- WorldLevel.js
+++ WorldLevel.js
@@ -1,14 +1,15 @@
 class WorldLevel {
-  constructor(data) {
-    this.data = data;
+  constructor(json) {
+    this.schemaVersion = json.schemaVersion ?? 1;
 
-    this.w = data.world.w;
-    this.h = data.world.h;
-    this.camLerp = data.camera.lerp;
+    this.w = json.world?.w ?? 2400;
+    this.h = json.world?.h ?? 1600;
 
-    this.flowers = [];
+    this.camLerp = json.camera?.lerp ?? 0.12;
 
     // Generate flowers
+    this.flowers = [];
+
     for (let i = 0; i < 350; i++) {
       this.flowers.push({
         x: random(this.w),
@@ -25,23 +26,30 @@ class WorldLevel {
     }
   }
 
+  // Soft screen background
   drawBackground() {
-    for (let y = 0; y < height; y++) {
-      let inter = map(y, 0, height, 0, 1);
-      let c = lerpColor(color(180, 225, 255), color(255, 245, 220), inter);
-      stroke(c);
-      line(0, y, width, y);
-    }
+    background(171, 214, 172); // #ABD6AC base
   }
 
+  // Meadow world
   drawWorld() {
-    // Grass base
     noStroke();
-    fill(126, 217, 87);
+
+    // Dark green base
+    fill(110, 170, 110);
     rect(0, 0, this.w, this.h);
 
+    // Soft lighter glow in center
+    let centerX = this.w / 2;
+    let centerY = this.h / 2;
+
+    fill(171, 214, 172, 180);
+    ellipse(centerX, centerY, this.w * 0.9, this.h * 0.9);
+
+    fill(171, 214, 172, 120);
+    ellipse(centerX, centerY, this.w * 0.6, this.h * 0.6);
+
     this.drawFlowers();
-    this.drawGrassSway();
   }
 
   drawFlowers() {
@@ -53,22 +61,24 @@ class WorldLevel {
       push();
       translate(f.x, f.y + floatY);
 
-      fill(...f.col);
-      ellipse(0, 0, f.size);
+      // Draw 5 petals
+      let petalCount = 5;
+      let radius = f.size / 2;
 
+      fill(...f.col);
+      for (let i = 0; i < petalCount; i++) {
+        let angle = (TWO_PI / petalCount) * i;
+        let petalX = cos(angle) * radius;
+        let petalY = sin(angle) * radius;
+
+        ellipse(petalX, petalY, radius, radius * 1.2);
+      }
+
+      // Draw center
       fill(255, 240, 150);
-      ellipse(0, 0, f.size / 2);
+      ellipse(0, 0, radius, radius);
 
       pop();
-    }
-  }
-
-  drawGrassSway() {
-    stroke(100, 190, 100, 120);
-
-    for (let i = 0; i < this.w; i += 25) {
-      let sway = sin(frameCount * 0.01 + i * 0.1) * 6;
-      line(i, this.h, i + sway, this.h - 35);
     }
   }
 }
